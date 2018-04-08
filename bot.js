c312abed1c0f3c0ed48ea7f58efa03538df63d8d
TheFatSoviet <32907174+TheFatSoviet@users.noreply.github.com>
2018-04-08 15:55:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
- Discord = require('discord.js');
+Discord = require('discord.js');
 const client = new Discord.Client();
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -16,18 +20,41 @@ client.on('message', message => {
     	message.reply('BONG!');
   	}
 });
+
 client.on('message', message => {
-    if (message.content === '!Hporn') {
-    function getRandomSite(){
-   var sites = ["google.com","bing.com","xyz.com","abc.com","example.com"];
-    var i = parseInt(Math.random()*(sites.length-1));
-    return sites[i];
-};
-    	
+    if (message.content === 'test') {
+    	message.reply('NTM!');
   	}
 });
+
 client.on('message', message => {
     if (message.content === 't2') {
+    	message.reply('t3 mdr');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '!Hporn') {
+     var nombre_de_phrases = 4; // le nombre de phrases que tu vas mettre (PS : on compte à partir de 1)
+     switch (getRandomInt(nombre_de_phrases)) {
+       // Toutes les phrases en fonction des cas (PS : on compte à partir de 0)
+      case 0:
+       message.reply('http://www.comptoir-hardware.com/images/stories/_modding/parvumsystems-projectmagnus/modding-parvum-systems-project-magnus-08_t.jpg');
+       break;
+      case 1:
+       message.reply('https://www.cowcotland.com/images/news/2016/05/mod-apex-pc-sobre-mais-diablement-realise.jpg');
+       break;
+      case 2:
+       message.reply('http://www.overclockingmadeinfrance.com/wp-content/uploads/2016/01/Kryptek-PC-Project-Pyrrus-2.jpg');
+       break;
+      case 3:
+       message.reply('https://www.materiel.net/live/403467.jpg');
+       break;
+      // Au cas  où rien (on sait jamais)
+      default:
+       message.reply('lol');
+       break;
+     }
     	message.reply('t3 mdr');
   	}
 });
